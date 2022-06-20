c118db2e8102d033d8abb8663722c9e019561d13
ranamudassir <ranamudassir964@gmail.com>
2022-06-21 01:19:55 +0500

    initialcommit

--- app/src/main/java/com/example/diapredict/MainActivity.kt
+++ app/src/main/java/com/example/diapredict/MainActivity.kt
@@ -9,6 +9,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        imageView4.setOnClickListener {
+            val intent = Intent(this, Dashboard::class.java)
+            startActivity(intent)
+        }
+
         button.setOnClickListener {
             val intent = Intent(this, show::class.java)
             startActivity(intent)
--- app/src/main/java/com/example/diapredict/show.kt
+++ app/src/main/java/com/example/diapredict/show.kt
@@ -1,11 +1,18 @@
 package com.example.diapredict
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_show.*
 
 class show : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show)
+        analyze.setOnClickListener {
+            val intent = Intent(this, show::class.java)
+            startActivity(intent)
+        }
     }
 }
